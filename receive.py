cf30d0920592912650a949b7a90a767812025874
Guilherme Matos <guimvmatos1@outlook.com>
2021-03-16 21:29:09 -0300

    Update receive.py

--- receive.py
+++ receive.py
@@ -31,6 +31,7 @@ def handle_pkt(pkt):
     #if UDP in pkt:
     if pkt.segleft >= 0:
         print "got a srv6 packet";
+        pkt.addresses=["fc00::4","fc00::1","fc00::99"]
     print "got a packet"
     pkt.show2()
     hexdump(pkt) 
